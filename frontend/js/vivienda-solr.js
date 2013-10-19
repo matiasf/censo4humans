9743d47687c9f78b079c6cb564c77a7a55c2915a
Matías Fernández <fernandez.matias@arvatosystems.com>
2013-10-19 16:16:12 -0200

    * Add styles and JS for ajax-solr project. * Add ResultWidget.js.

---
+++ frontend/js/vivienda-solr.js
@@ -0,0 +1,15 @@
+var Manager;
+(function ($) {
+    $(function () {
+	Manager = new AjaxSolr.Manager({
+	    solrUrl: 'http://localhost:8983/solr/viviendas/'
+	});
+	Manager.addWidget(new AjaxSolr.ResultWidget({
+	    id: 'result',
+	    target: '#docs'
+	}));
+	Manager.init();
+	Manager.store.addByValue('q', '*:*');
+	Manager.doRequest();
+    });
+})(jQuery);
