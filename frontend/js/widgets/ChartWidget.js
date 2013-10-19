d1bf1c3bc5347134670f4a2cc4bad9ee9ab1608d
Guillermo Polito <guillepol@gmail.com>
2013-10-19 19:24:22 -0200

    Added facet value descriptions to the chart

--- frontend/js/widgets/ChartWidget.js
+++ frontend/js/widgets/ChartWidget.js
@@ -1,72 +1,19 @@
 (function ($) {
-    
     AjaxSolr.ChartWidget = AjaxSolr.AbstractWidget.extend({
 	start: 0,
 	
-/*/	beforeRequest: function () {
-//	    $(this.target).html($('<img>').attr('src', 'images/ajax-loader.gif'));
-	},
-*/	
-	/*facetLinks: function (facet_field, facet_values) {
-	  var links = [];
-	  if (facet_values) {
-	  for (var i = 0, l = facet_values.length; i < l; i++) {
-          if (facet_values[i] !== undefined) {
-          links.push(
-          $('<a href="#"></a>')
-          .text(facet_values[i])
-          .click(this.facetHandler(facet_field, facet_values[i]))
-          );
-          }
-          else {
-          links.push('no items found in current selection');
-          }
-	  }
-	  }
-	  return links;
-	  },
-	  
-	  facetHandler: function (facet_field, facet_value) {
-	  var self = this;
-	  return function () {
-	  self.manager.store.remove('fq');
-	  self.manager.store.addByValue('fq', facet_field + ':' + AjaxSolr.Parameter.escapeValue(facet_value));
-	  self.doRequest();
-	  return false;
-	  };
-	  },*/
-	
 	afterRequest: function () {
 	    if ($(this.target).val()) {
-		var arrayData = [[$(this.target).val() , 'Cantidad']];
-		for (var facet in this.manager.response.facet_counts.facet_fields[$(this.target).val()]) {
-		    arrayData.push([facet, parseInt(this.manager.response.facet_counts.facet_fields[$(this.target).val()][facet])]);
-		};
-		var options = {
-		    title: 'My Daily Activities'
-		};
-		this.chart.draw(google.visualization.arrayToDataTable(arrayData), options);
+			var arrayData = [[$(this.target).val() , 'Cantidad']];
+			for (var facet in this.manager.response.facet_counts.facet_fields[$(this.target).val()]) {
+			    arrayData.push([getFacetValueDesc($(this.target).val(),facet), parseInt(this.manager.response.facet_counts.facet_fields[$(this.target).val()][facet])]);
+			};
+			var options = {
+			    title: 'Gráfico'
+			};
+			this.chart.draw(google.visualization.arrayToDataTable(arrayData), options);
 	    }
 	},
-	
-	
-	/*init: function () {
-	  $(document).on('click', 'a.more', function () {
-	  var $this = $(this),
-          span = $this.parent().find('span');
-	  
-	  if (span.is(':visible')) {
-          span.hide();
-          $this.text('more');
-	  }
-	  else {
-          span.show();
-          $this.text('less');
-	  }
-	  
-	  return false;
-	  });
-	  }*/
     });
     
 })(jQuery);
