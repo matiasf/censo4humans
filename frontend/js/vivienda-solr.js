e6684553ee1bfea1a2188226553381cbda09bf0f
Guillermo Polito <guillepol@gmail.com>
2013-10-19 16:57:56 -0200

    Added Facet and CurrentSearch widgets

--- frontend/js/vivienda-solr.js
+++ frontend/js/vivienda-solr.js
@@ -8,8 +8,35 @@ var Manager;
 	    id: 'result',
 	    target: '#docs'
 	}));
+
+	var fields = [ 'DPTO', 'LOC', 'CCZ' ];
+	for (var i = 0, l = fields.length; i < l; i++) {
+	  Manager.addWidget(new AjaxSolr.FacetWidget({
+	    id: fields[i],
+	    target: '#' + fields[i],
+	    field: fields[i]
+	  }));
+	}
+	Manager.addWidget(new AjaxSolr.CurrentSearchWidget({
+	  id: 'currentsearch',
+	  target: '#selection',
+	}));
+
 	Manager.init();
 	Manager.store.addByValue('q', '*:*');
+
+	var params = {
+      facet: true,
+      'facet.field': [ 'DPTO', 'LOC', 'CCZ' ],
+      'facet.limit': 20,
+      'facet.mincount': 1,
+      'f.topics.facet.limit': 50,
+      'json.nl': 'map'
+    };
+    for (var name in params) {
+      Manager.store.addByValue(name, params[name]);
+    }
+
 	Manager.doRequest();
     });
 })(jQuery);
