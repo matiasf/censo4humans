ac824053b0ba98b3434a6f2c395e592b6431344e
Matías Fernández <recta.de.euler@gmail.com>
2014-08-03 17:37:40 -0300

    Add GPL3 licence on the program.

--- escribicenso/src/uy/com/leecenso/DiccionarioVariablesHelper.java
+++ escribicenso/src/uy/com/leecenso/DiccionarioVariablesHelper.java
@@ -1,3 +1,20 @@
+/**
+ *   Censo 4 Humans is a web app to provide census data in a friendly way.
+ *   Copyright (C) 2013  Guillermo Polito, Guillermo Suarez, Matías Fernández
+ *
+ *   This program is free software: you can redistribute it and/or modify
+ *   it under the terms of the GNU General Public License as published by
+ *   the Free Software Foundation, either version 3 of the License, or
+ *   (at your option) any later version.
+ *
+ *   This program is distributed in the hope that it will be useful,
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *   GNU General Public License for more details.
+ *
+ *   You should have received a copy of the GNU General Public License
+ *   along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
 package uy.com.leecenso;
 
 import java.io.BufferedWriter;
